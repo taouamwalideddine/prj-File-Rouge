8eb956ff48ffb4844ba7fa4216a3d0beca73d056
taouamwalideddine <wtaouam@gmail.com>
2025-04-11 15:21:49 +0100

    werking on routter

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,18 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
 });
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/dashboard', fn() => view('admin.dashboard'))->name('admin.dashboard');
-});
 
-Route::middleware(['auth', 'teacher'])->group(function () {
-    Route::get('/teacher/dashboard', fn() => view('teacher.dashboard'))->name('teacher.dashboard');
-});
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/login', 'showLoginForm')->name('login');
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->name('logout');
 
-Route::middleware(['auth', 'student'])->group(function () {
-    Route::get('/student/dashboard', fn() => view('student.dashboard'))->name('student.dashboard');
+    Route::get('/register', 'showRegistrationForm')->name('register');
+    Route::post('/register', 'register');
 });
