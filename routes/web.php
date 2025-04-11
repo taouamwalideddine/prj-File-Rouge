cf5be78157f5710c868bb1a9a8cd65f80f1127da
taouamwalideddine <wtaouam@gmail.com>
2025-04-11 15:16:59 +0100

    fex middleware

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,27 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
-// auth
-Route::controller(LoginController::class)->group(function () {
-    Route::get('login', 'showLoginForm')->name('login');
-    Route::post('login', 'login');
-    Route::post('logout', 'logout')->name('logout');
-});
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureIsAdmin::class,
+            'teacher' => \App\Http\Middleware\EnsureIsTeacher::class,
+            'student' => \App\Http\Middleware\EnsureIsStudent::class,
+        ]);
 
-Route::controller(RegisterController::class)->group(function () {
-    Route::get('register', 'showRegistrationForm')->name('register');
-    Route::post('register', 'register');
-});
-// teeacher
-Route::middleware(['auth', 'teacher'])->group(function () {
-    Route::get('/teacher/dashboard', function () {
-        return view('teacher.dashboard');
-    })->name('teacher.dashboard');
-});
-// student
-Route::middleware(['auth', 'student'])->group(function () {
-    Route::get('/student/dashboard', function () {
-        return view('student.dashboard');
-    })->name('student.dashboard');
-});
+        $middleware->web([
+            // 
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
