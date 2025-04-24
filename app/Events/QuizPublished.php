8c86233a7a8cece546a474131cfd4adab5fc7d52
taouamwalideddine <wtaouam@gmail.com>
2025-04-25 00:06:48 +0100

    added logic to QuizPublished event

--- app/Events/QuizPublished.php
+++ app/Events/QuizPublished.php
@@ -2,35 +2,32 @@
 
 namespace App\Events;
 
+use App\Models\Quiz;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
-class QuizPublished
+class QuizPublished implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, InteractsWithSockets;
 
-    /**
-     * Create a new event instance.
-     */
-    public function __construct()
+    public $quiz;
+    public $message;
+
+    public function __construct(Quiz $quiz)
     {
-        //
+        $this->quiz = $quiz;
+        $this->message = "New quiz available: {$quiz->title}";
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
+    public function broadcastOn()
     {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
+        return new Channel('classroom.' . $this->quiz->classroom_id);
+    }
+
+    public function broadcastAs()
+    {
+        return 'quiz.published';
     }
 }
